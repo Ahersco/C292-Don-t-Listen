8e1622c247afc6284659a9e5d5ae95dbb672548d
Ahersco <aaron@herscovici.com>
2025-10-07 18:38:43 -0400

    start

---
+++ Lab13/Assets/Script/Player_Script.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Script : MonoBehaviour
+{
+    [SerializeField] float JumpForce;
+    [SerializeField] float speed2;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] GameObject Button;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Button.activeSelf == false)
+        {
+            float x = Input.GetAxis("Horizontal");
+            Vector2 moveVector = new Vector2(x, 0);
+            transform.Translate(moveVector * speed2 * Time.deltaTime);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            }
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            Button.SetActive(true);
+        }
+        if (collision.gameObject.tag == "Finish")
+        {
+            Destroy(collision.gameObject);
+            Button.SetActive(true);
+        }
+    }
+}
