415122b3a7e97210ac544c7e677ec319c0ea6b65
Ahersco <aaron@herscovici.com>
2025-11-20 20:18:54 -0500

    level 8 done

--- Lab13/Assets/Script/platform_script.cs
+++ Lab13/Assets/Script/platform_script.cs
@@ -1,25 +1,50 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class platform_script : MonoBehaviour
 {
     [SerializeField] float speed2;
+    [SerializeField] float speed;
     [SerializeField] float x;
     public bool isMoving;
+    [SerializeField] bool Xaxis;
+    [SerializeField] bool Yaxis;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (SceneManager.GetActiveScene().name == "Level8")
+        {
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+            {
+                speed2 = speed2 * 4;
+            }
+            else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+            {
+                speed2 = speed2 / 4;
+            }
+        }
         if (isMoving == true)
         {
-            transform.Translate(Vector2.left * speed2 * Time.deltaTime);
+            if (Xaxis == true)
+            {
+                transform.Translate(Vector2.left * speed * Time.deltaTime);
+            }
+            if (Yaxis == true)
+            {
+                transform.Translate(Vector2.down * speed2 * Time.deltaTime);
+            }
         }
-        if (transform.position.x <= x)
+        if (transform.position.x <= x && Xaxis == true)
+        {
             isMoving = false;
+        }
     }
     
     
